c19883bdf523ce78b50f4c7d11ab86835920b155
bribrisleepy <briannakim03@gmail.com>
2021-08-26 19:39:56 +1000

    navigation bar

--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -1,27 +1,22 @@
-import SwiftUI
+//
+//  ListView.swift
+//  ToDoList
+//
+//  Created by Borborick Zhu on 8/7/21.
+//  Home page view
 
-struct ToDoButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .padding()
-            .background(Color("RegularListColor").opacity(configuration.isPressed ? 1 : 0.5))
-            .clipShape(Capsule())
-    }
-}
+import SwiftUI
 
 struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     @ObservedObject var userSettings = UserModel()
-    
-    @State private var todayListClicked = false
-    @State private var weeklyListClicked = false
    
     let today = Date()
     // let aWeekLater = Calendar.current.date(byAdding: dateComponent.day = 7, to: today)
     
     var body: some View {
-        VStack {
-           // if userSettings.selectedView == "Daily" {
+        NavigationView {
+            if userSettings.selectedView == "Daily" {
                 // Set up for daily view
                 VStack (alignment: .leading) {
                     Text("Hello \(userSettings.name),")
@@ -35,66 +30,18 @@ struct ListView: View {
                         .italic()
                         .font(.footnote)
                 }
-                .offset(x: 20)
-            HStack {
-                Button ("Today") {
-                    todayListClicked = true
-                    weeklyListClicked = false
-                    listViewModel.orderDailyTasks()
-                }
-                .buttonStyle(ToDoButton())
-                
-                Button ("This Week") {
-                    todayListClicked = false
-                    weeklyListClicked = true
-                    listViewModel.orderWeeklyTasks()
-                }
-                .buttonStyle(ToDoButton())
-                
-                Button("All Tasks") {
-                    todayListClicked = false
-                    weeklyListClicked = false
-                }
-                .buttonStyle(ToDoButton())
-            }
-            
-            if todayListClicked {
-                    List {
-                        // Section 1: Recurring Features
-                        ForEach(listViewModel.ordDailyItems) { item in
-                            ListRowView(item: item)
-                                .onTapGesture {
-                                    listViewModel.updateRecItem(recItem: item)
-                                }
+                    .navigationBarItems(
+                        leading: NavigationLink(destination: SettingsView()) {
+                            Text("Settings")
+                        },
+                        trailing: NavigationLink(destination: AddView()) {
+                            Text("+")
                         }
-                        .onDelete(perform: listViewModel.deleteRecItem)
-                        .onMove(perform: listViewModel.moveRecItem)
-                    }
-                    .listStyle(PlainListStyle())
-                    .padding(30)
-            }
-
-            else if weeklyListClicked {
-                    List {
-                        // Section 1: Recurring Features
-                        ForEach(listViewModel.ordWeeklyItems) { item in
-                            ListRowView(item: item)
-                                .onTapGesture {
-                                    listViewModel.updateRecItem(recItem: item)
-                                }
-                        }
-                        .onDelete(perform: listViewModel.deleteRecItem)
-                        .onMove(perform: listViewModel.moveRecItem)
-                    }
-                    .listStyle(PlainListStyle())
-                    .padding(30)
-                    
-            }
-            
-            else {
+                    )
+                    .offset(x: 20)
+                
                 List {
-                    // Section 1: Recurring Features
-                    ForEach(listViewModel.allItems) { item in
+                    ForEach(listViewModel.ordDailyItems) { item in
                         ListRowView(item: item)
                             .onTapGesture {
                                 listViewModel.updateRecItem(recItem: item)
@@ -103,24 +50,10 @@ struct ListView: View {
                     .onDelete(perform: listViewModel.deleteRecItem)
                     .onMove(perform: listViewModel.moveRecItem)
                 }
-                .listStyle(PlainListStyle())
-                .padding(30)
-            }
-            
-            NavigationLink(
-                destination: AddView(),
-                label: {
-                    Text("Add To-Do ✏️")
-                        .font(.headline).italic()
-                        .foregroundColor(.black).opacity(1.0)
-                        .padding()
-                        .background(Color("RecurringListColor").opacity(0.6))
-                        .cornerRadius(20)
-                }
-          )
-
-            //.offset(y: -100)
-            /*} else if userSettings.selectedView == "Weekly" {
+                    .listStyle(PlainListStyle())
+                    .padding(30)
+                    .offset(y: -100)
+            } else if userSettings.selectedView == "Weekly" {
                 // Set up for weekly view
                 VStack (alignment: .leading) {
                     Text("Hello \(userSettings.name),")
@@ -134,6 +67,14 @@ struct ListView: View {
                         .italic()
                         .font(.footnote)
                 }
+                    .navigationBarItems(
+                        leading: NavigationLink(destination: SettingsView()) {
+                            Text("Settings")
+                        },
+                        trailing: NavigationLink(destination: AddView()) {
+                            Text("+")
+                        }
+                    )
                     .offset(x: 20)
                 List {
                     // Section 1: Recurring Features
@@ -149,15 +90,18 @@ struct ListView: View {
                     .listStyle(PlainListStyle())
                     .padding(30)
                     .offset(y: -100)
-            }*/
-            
+            }
         }
     }
 }
 
 struct ListView_Previews: PreviewProvider {
     static var previews: some View {
-        ListView()
-            .environmentObject(ListViewModel())
+        Group {
+            ListView()
+                .environmentObject(ListViewModel())
+            ListView()
+                .environmentObject(ListViewModel())
+        }
     }
 }
