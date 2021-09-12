879e17c6f0c102c7d3c4b0b8c3e6a16ffc36901b
bribrisleepy <briannakim03@gmail.com>
2021-09-12 17:31:43 +1000

    updated user model

--- ViewModels/UserModel.swift
+++ ViewModels/UserModel.swift
@@ -22,21 +22,11 @@ class UserModel: ObservableObject {
             UserDefaults.standard.set(isDarkMode, forKey: "isDarkMode")
         }
     }
-    
-    // Saves user's preferred view
-    @Published var selectedView: String {
-        didSet {
-            UserDefaults.standard.set(selectedView, forKey: "selectedView")
-        }
-    }
-    
-    public var views = ["Daily", "Weekly", "All Tasks"]
         
     init() {
         self.name = UserDefaults.standard.object(forKey: "name") as? String ?? ""
         self.isSetUp = UserDefaults.standard.object(forKey: "isSetUp") as? Bool ?? false
         self.isDarkMode = UserDefaults.standard.object(forKey: "isDarkMode") as? Bool ?? false
-        self.selectedView = UserDefaults.standard.object(forKey: "selectedView") as? String ?? "Daily"
     }
 }
 
